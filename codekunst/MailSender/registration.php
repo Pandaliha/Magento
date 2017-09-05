1d74aef926e3dc2e108b7e2b7faa674b0376962a
Pandaliha <25638438+Pandaliha@users.noreply.github.com>
2017-09-05 17:38:31 +0200

    Order On Hold - Custom Email NOT sent - New order Email sent

---
+++ codekunst/MailSender/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'codekunst_MailSender',
+    __DIR__
+);
--- codekunst/Orderhold/Observer/ChangeState.php
+++ codekunst/Orderhold/Observer/ChangeState.php
@@ -38,35 +38,7 @@ class ChangeState implements \Magento\Framework\Event\ObserverInterface {
 
         $order = $observer->getEvent()->getOrder();
         $order_id = $order->getID();
-        //$order = $observer->getOrder();
-
-        //order can't be placed at all
-		//try {
-			//$this->orderHelper->hold($order);
-		// }
-		// catch (\Exception $e) {
-		// 	$this->logger->debug($e->__toString());
-		// }
-
-    //Version 1
-            // $order->setState(\Magento\Sales\Model\Order::STATE_HOLDED);
-            // $order->setStatus($order->getConfig()->getStateDefaultStatus(\Magento\Sales\Model\Order::STATE_HOLDED));
-
-
-          //$order->setCustomerIsGuest(false)->setCustomerId($customer->getId())->setCustomerEmail($customer->getEmail());
-//Version 2
-          // place order button causes infinite loading and order isn't placed
-            // $order->setState(Mage_Sales_Model_Order::STATE_HOLDED, true);
-            // $order->save();
-
-            //redirects (wegen die) to cart ABER SETZT BESTELLUNG ON HOLD, sendet keine email
-            //if($order->canHold()) {
-
             $order->hold()->save();
 
-          //}
-
-
-        //die();
     }
 }
