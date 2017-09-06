df4f94ba9d69b9503dc740d393c0e8988a9dcad2
Pandaliha <25638438+Pandaliha@users.noreply.github.com>
2017-09-06 16:09:56 +0200

    Order not saved - no Email sent - Place Order: Page loads endlessly

--- codekunst/Orderhold/Model/MailSender.php
+++ codekunst/Orderhold/Model/MailSender.php
@@ -1,15 +1,13 @@
 <?php
+namespace codekunst\Orderhold\Model;
 
-namespace codekunst\Orderhold;
-
-use Magento\Framework\ObjectManager\ObjectManager;
-use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Mail\Template\TransportBuilder;
 use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
-use codekunst\Orderhold\Model\MailSender;
+use Magento\Backend\App\Action\Context;
 
-class ChangeState {
+class SendMail extends \Magento\Backend\App\Action {
+    protected $_orderRepositoryInterface;
     /**
      * @var \Magento\Store\Model\StoreManagerInterface
      */
@@ -18,21 +16,18 @@ class ChangeState {
      * @var \Magento\Framework\Mail\Template\TransportBuilder
      */
     protected $_transportBuilder;
-
-
     /** @var \Magento\Framework\Logger\Monolog */
     protected $_logger;
-
     /**
      * @var \Magento\Framework\ObjectManager\ObjectManager
      */
     protected $_objectManager;
-
     protected $_orderFactory;
     protected $_checkoutSession;
+    protected $request;
     protected $scopeConfig;
-
     public function __construct(
+        Context $context,
         \Magento\Customer\Api\CustomerRepositoryInterface $customerRepositoryInterface,
         \Magento\Customer\Model\Customer $customer,
         \Psr\Log\LoggerInterface $logger,
@@ -42,15 +37,10 @@ class ChangeState {
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\App\Request\Http $request,
         ScopeConfigInterface $scopeConfig,
-        MailSender $mailSender,
-
-
-
         \Psr\Log\LoggerInterface $loggerInterface,
-        \Magento\Checkout\Model\Session $checkoutSession,
-        \Magento\Sales\Model\OrderFactory $orderFactory,
-        ObjectManager $objectManager
+        \Magento\Framework\ObjectManager\ObjectManager $objectManager
     ) {
+        parent::__construct($context);
         $this->_customerRepositoryInterface = $customerRepositoryInterface;
         $this->_orderRepositoryInterface = $orderRepositoryInterface;
         $this->logger = $logger;
@@ -59,31 +49,36 @@ class ChangeState {
         $this->request = $request;
         $this->_logger = $loggerInterface;
         $this->_objectManager = $objectManager;
-
-
-
-
-
-        $this->_logger = $loggerInterface;
-        $this->_objectManager = $objectManager;
-        $this->_orderFactory = $orderFactory;
-        $this->_checkoutSession = $checkoutSession;
-        $this->mailSender = $mailSender;
     }
-
     /**
      *
      *
      * @param \Magento\Framework\Event\Observer $observer
      */
-    public function execute(\Magento\Framework\Event\Observer $observer ) {
-
-        $order = $observer->getEvent()->getOrder();
-        $this->mailSender->execute();
-
-        $order->hold()->save();
-
-
-
+    public function execute() {
+        if ($this instanceof \Magento\Sales\Model\Order) {
+            $order = $this;
+            $emailCust = $order->getCustomerEmail();
+            $email = $this->scopeConfig->getValue('trans_email/ident_general/email', ScopeInterface::SCOPE_STORE);
+            $name = $this->scopeConfig->getValue('trans_email/ident_general/name', ScopeInterface::SCOPE_STORE);
+            $sender = [
+                'name' => $email,
+                'email' => $name,
+            ];
+            $transport = $this->_transportBuilder
+                ->setTemplateIdentifier('myemail_template')
+                ->setTemplateOptions(['area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+                    'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID])
+                ->setTemplateVars(
+                    [
+                        'store' => $this->_storeManager->getStore(),
+                    ]
+                )
+                ->setFrom($sender)
+                ->addTo($emailCust)
+                ->getTransport();
+            $transport->sendMessage();
+        }
+         return $this;
     }
 }
