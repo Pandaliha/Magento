e84ba440c2014db1051ee9cc1c518cbf37d710d8
Pandaliha <25638438+Pandaliha@users.noreply.github.com>
2017-09-12 15:44:53 +0200

    Successpage loads - ConfirmOrder Mail sends - New Order Mail sends - Order on hold

--- codekunst/Dropshipping/Model/Carrier/Method.php
+++ codekunst/Dropshipping/Model/Carrier/Method.php
@@ -3,7 +3,6 @@ namespace codekunst\Dropshipping\Model\Carrier;
 
 use Magento\Quote\Model\Quote\Address\RateRequest;
 use Magento\Shipping\Model\Rate\Result;
-use Magento\Checkout\Helper\Cart;
 
 /**
  * Extends AbstractCarrier
@@ -11,6 +10,9 @@ use Magento\Checkout\Helper\Cart;
 class Method extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
     \Magento\Shipping\Model\Carrier\CarrierInterface
 {
+    protected $cartHelper;
+
+
     /**
      * @var string
      */
@@ -41,6 +43,7 @@ class Method extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
      * @param array $data
      */
     public function __construct(
+        \Magento\Checkout\Helper\Cart $cartHelper,
         \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
         \Magento\Quote\Model\Quote\Address\RateResult\ErrorFactory $rateErrorFactory,
         \Psr\Log\LoggerInterface $logger,
@@ -48,6 +51,7 @@ class Method extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
         \Magento\Quote\Model\Quote\Address\RateResult\MethodFactory $rateMethodFactory,
         array $data = []
     ) {
+        $this->cartHelper = $cartHelper;
         $this->_rateResultFactory = $rateResultFactory;
         $this->_rateMethodFactory = $rateMethodFactory;
         parent::__construct($scopeConfig, $rateErrorFactory, $logger, $data);
@@ -56,29 +60,29 @@ class Method extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
     /**
      * @param RateRequest $request
      * @return Result|bool
-     * @SuppressWarnings(PHPMD.CyclomaticComplexity)
-     * @SuppressWarnings(PHPMD.NPathComplexity)
+     *
      */
     //  shipping information as parameter and returns all available rates
     public function collectRates(RateRequest $request)
     {
+        /*
+         * Shipping only available if configflag set on active
+         *
+         * */
         if (!$this->getConfigFlag('active')) {
             return false;
         }
 
-
-
-        /** @var Result $result */
+        /** Calculating the price of shipping
+         * @var Result $result
+         */
         $result = $this->_rateResultFactory->create();
         if ($this->getConfigData('type') == 'O') {
             // per order
             $shippingPrice = $this->getConfigData('price');
         } elseif ($this->getConfigData('type') == 'I') {
             // per item
-            $count = $request->getPackageQty();
             $shippingPrice = $request->getPackageQty() * $this->getConfigData(
-                'price'
-            ) - $this->getFreeBoxes() * $this->getConfigData(
                 'price'
             );
         } else {
@@ -87,7 +91,10 @@ class Method extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
 
         $shippingPrice = $this->getFinalPriceWithHandlingFee($shippingPrice);
 
-        if ($shippingPrice !== false && $count < 6) {
+        /*
+         * Only offer Dropshipping option if there are less than 6 Items bought
+         * */
+        if ($this->cartHelper->getSummaryCount() < 6) {
             /** @var \Magento\Quote\Model\Quote\Address\RateResult\Method $method */
             $method = $this->_rateMethodFactory->create();
 
@@ -99,10 +106,7 @@ class Method extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
 
             $method->setPrice($shippingPrice);
             $method->setCost($shippingPrice);
-
-
-                $result->append($method);
-                //return false; verhindert alle Shipping Optionen
+            $result->append($method);
 
 
         }
@@ -115,31 +119,6 @@ class Method extends \Magento\Shipping\Model\Carrier\AbstractCarrier implements
      */
     public function getAllowedMethods()
     {
-      // TODO limit dropshipping for number of items
-      //IF MORE THAN 5 ITEMS; SET ACTIVE TO 0
-
-      // $cartItemsCount = Mage::helper('checkout/cart')->getCart()->getItemsCount();
-
-      // $count = $this->helper('checkout/cart')->getSummaryCount();
-
-      //$this -> getConfigData('active')->saveConfig('active', '0', 'default', 0);
-
-    //  $counter = $this->helper('\Magento\Checkout\Helper\Cart');
-      //Mage::helper('checkout/cart')->getSummaryCount()
-
-      // $noOfItems = $counter->getSummaryCount();
-      //     if($noOfItems > 5){
-      //       $this->setConfigData('codekunst_Dropshipping/etc/config', 'A', 'stores', 1);
-
-      // Mage::helper('checkout/cart')->getSummaryCount();
-
-      // $count = $this->helper('checkout/cart')->getSummaryCount();
-
-
-      // $count = $this->getSingleton('checkout/cart')->getSummaryCount();
-      //
-      //   if($count < 5){
         return ['shippingmethod' => $this->getConfigData('name')];
-      //}
 }
 }
--- codekunst/Orderhold/Observer/ChangeState.php
+++ codekunst/Orderhold/Observer/ChangeState.php
@@ -4,8 +4,6 @@ use Magento\Framework\App\Config\ScopeConfigInterface;
 use Magento\Store\Model\ScopeInterface;
 use Magento\Framework\Mail\Template\TransportBuilder;
 class ChangeState implements \Magento\Framework\Event\ObserverInterface {
-
-
     /**
      * @var \Magento\Store\Model\StoreManagerInterface
      */
@@ -14,84 +12,77 @@ class ChangeState implements \Magento\Framework\Event\ObserverInterface {
      * @var \Magento\Framework\Mail\Template\TransportBuilder
      */
     protected $_transportBuilder;
-
     /** @var \Magento\Framework\Logger\Monolog */
     protected $_logger;
     /**
      * @var \Magento\Framework\ObjectManager\ObjectManager
      */
     protected $_objectManager;
-
     protected $_checkoutSession;
     protected $scopeConfig;
-    public function __construct(
 
+    public function __construct(
         \Magento\Customer\Model\Customer $customer,
         TransportBuilder $transportBuilder,
         \Magento\Store\Model\StoreManagerInterface $storeManager,
         \Magento\Framework\App\Action\Context $context,
-
         ScopeConfigInterface $scopeConfig,
         \Psr\Log\LoggerInterface $loggerInterface,
         \Magento\Checkout\Model\Session $checkoutSession,
         \Magento\Framework\ObjectManager\ObjectManager $objectManager
     ) {
-
         $this->customer =$customer;
         $this->storeManager = $storeManager;
-
         $this->_transportBuilder = $transportBuilder;
         $this->scopeConfig = $scopeConfig;
         $this->_logger = $loggerInterface;
         $this->_objectManager = $objectManager;
         $this->_checkoutSession = $checkoutSession;
     }
-
     public function sendEmail($name, $email, $custEmail, $data)
     {
-            $sender = [
-                'name' => $email,
-                'email' => $name,
-            ];
+        $sender = [
+            'name' => $email,
+            'email' => $name,
+        ];
         $postObject = new \Magento\Framework\DataObject();
         $postObject->setData($data);
 
-            $transport = $this->_transportBuilder
-                ->setTemplateIdentifier('myemail_email_template')
-                ->setTemplateOptions(['area' => \Magento\Framework\App\Area::AREA_FRONTEND,
-                    'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID])
-                ->setTemplateVars(['data' => $postObject])
-                ->setFrom($sender)
-                ->addTo($custEmail)
-                ->getTransport();
-            $transport->sendMessage();
-            return $this;
-        }
-
+        $transport = $this->_transportBuilder
+            ->setTemplateIdentifier('myemail_email_template')
+            ->setTemplateOptions(['area' => \Magento\Framework\App\Area::AREA_FRONTEND,
+                'store' => \Magento\Store\Model\Store::DEFAULT_STORE_ID,])
+            ->setTemplateVars(['data' => $postObject])
+            ->setFrom($sender)
+            ->addTo($custEmail)
+            ->getTransport();
+        $transport->sendMessage();
+        return $this;
+    }
     /**
      *
      *
      * @param \Magento\Framework\Event\Observer $observer
      */
     public function execute(\Magento\Framework\Event\Observer $observer ) {
-    $order = $observer->getEvent()->getOrder();
-    $orderID = $order->getIncrementId();
+        $order = $observer->getEvent()->getOrder();
+        $orderID = $order->getIncrementId();
+        $custEmail = $order->getCustomerEmail();
+        $custName = $order->getCustomerName();
+        $email = $this->scopeConfig->getValue('trans_email/ident_general/email', ScopeInterface::SCOPE_STORE);
+        $name = $this->scopeConfig->getValue('trans_email/ident_general/name', ScopeInterface::SCOPE_STORE);
 
-    $custEmail = $order->getCustomerEmail();
-    $custName = $order->getCustomerName();
-    $email = $this->scopeConfig->getValue('trans_email/ident_general/email', ScopeInterface::SCOPE_STORE);
-    $name = $this->scopeConfig->getValue('trans_email/ident_general/name', ScopeInterface::SCOPE_STORE);
-    $baseURL = $this->storeManager->getStore()->getBaseUrl();
-    $confirmURL = $baseURL . "confirm/Order/OrderConfirm/id/" . $orderID;
-
-    $data = [
-        'report_date' => date("j F Y", strtotime('-1 day')),
-        'name' => $custName,
-        'confirm' => $confirmURL
-    ];
-
-    $this->sendEmail($email, $name, $custEmail, $data);
-    $order->hold()->save();
-
+        $baseURL = $this->storeManager->getStore()->getBaseUrl();
+        $confirmURL = $baseURL . "confirm/Order/OrderConfirm/id/" . $orderID;
+        $data = [
+            'report_date' => date("j F Y", strtotime('-1 day')),
+            'name' => $custName,
+            'ordernum' => $orderID,
+            'confirm' => $confirmURL,
+            'shopowner' => $name
+        ];
+        $this->sendEmail($email, $name, $custEmail, $data);
+        $order->hold()->save();
+        //exit();
     }
 }
